7e3e2e99873633e36db44417e2a9c45e070b8759
root <root@dbsentey>
2014-02-10 04:25:24 -0600

    Added comments and removed debugging code.

--- modify.js
+++ modify.js
@@ -2,7 +2,7 @@
 
 /*
  * This script runs under node.js and will
- * modify our trimmed down jQuery 
+ * modify our trimmed down jQuery
  * renaming references to our custom name and removing
  * all references to the global $
  * Author: dbennett@bensoft.com
@@ -17,31 +17,31 @@ process.argv.forEach(function (val, index, array) {
 
 var rl = require('readline');
 
+// script is a line processor state machine - these are the states
 var state = {
-  curr: 0,
-  CODE: 0,
-  COMMENT: 1,
-  HEADER: 2,
-  LINE_COMMENT: 3,
-  NO_CONFLICT: 4
-}
+  curr: 0,				// current state
+  CODE: 0,				// code line
+  COMMENT: 1,			// comment line
+  HEADER: 2,			// header comment line
+  LINE_COMMENT: 3,		// line comment
+  NO_CONFLICT: 4		// noConflict method
+};
 
-// output a header 
+// output a header
 process.stdout.write("// NOTE: jQuery references have been changed to "+customName+" to avoid conflicts with existing configurations.\n\n");
 
 rl.createInterface({
     input: process.stdin,
     output: process.stdout
 }).on('line',function(l){
-  // do matching here
-  
+
   // if we are in code look for a header
   if (state.curr === state.CODE) {
     if (l.match(/\/\*/) != null) {
       state.curr=state.HEADER;
     }
   }
-  
+
   // if we are in a header and we see the end of comment then
   // go back to code
   if (state.curr === state.HEADER) {
@@ -49,61 +49,41 @@ rl.createInterface({
       state.curr=state.CODE;
     }
   }
-  
+
   // if we are in code and find // then it's a line comment
   if (state.curr === state.CODE) {
     if (l.match(/^\s*\/\//) != null) {
       state.curr=state.LINE_COMMENT;
     }
   }
-  
+
   // look for begining of noConflict
   if (state.curr === state.CODE) {
     if (l.match(/noConflict\s*:/) != null) {
       state.curr=state.NO_CONFLICT;
     }
   }
-  
-  // show if we are in CODE or HEADER
-//  switch (state.curr) {
-//    case state.CODE:
-//        process.stdout.write('CODE:');
-//      break;
-//    case state.HEADER:
-//        process.stdout.write('HEADER:');
-//        break;
-//    case state.LINE_COMMENT:
-//        process.stdout.write('LINE_COMMENT:');
-//      break;
-//    case state.NO_CONFLICT:
-//        process.stdout.write('NO_CONFLICT:');
-//      break;
-//    default:
-//      break;
-//  }
 
   // custom line replaces
   if (state.curr == state.CODE) {
-  
+
     // comment out _$ definition
     if (l.match(/_\$\s*=\s*window\.\$,/) != null) {
       l = l.replace(/^/,'//');
     }
-    
+
     if (l.match(/window\.jQuery\s*=\s*window\.\$\s*=\s*jQuery/) != null) {
       l = l.replace(/window\.jQuery\s*=\s*window\.\$\s*=\s*jQuery/,'window.jQuery = jQuery');
     }
-    
+
   }
 
-  
   // If we are in code or line_comment the rewrite jQuery references
   if (state.curr == state.CODE || state.curr == state.LINE_COMMENT) {
     l = l.replace(/jQuery/g,customName);
     l = l.replace(/jquery/g,customName.toLowerCase());
   }
-  
-  
+
   // output everything but noConflict function
   if (state.curr !== state.NO_CONFLICT) {
     process.stdout.write(l+"\n");
@@ -114,10 +94,10 @@ rl.createInterface({
     if (l.match(/},/) != null) {
       state.curr=state.CODE;
     }
-  }  
-  
+  }
+
   // if state is line comment then next line is code
   if (state.curr === state.LINE_COMMENT)
     state.curr = state.CODE;
-    
+
 });
